ceec9a59415ebafed1be7c0dff88491e81dea4f7
can <renkon1103@gmail.com>
2023-03-07 05:57:34 +0900

    timer実装

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,4 @@
-<script setup>
-import NowStatusFooter from './components/NowStatusFooter.vue'
-</script>
+
 
 <template>
   <header>
@@ -11,13 +9,14 @@ import NowStatusFooter from './components/NowStatusFooter.vue'
 
   <div class="d-flex align-items-center" style="height: 30vh">
     <div class="text-center w-100">
-      <div style="font-size: 5vh">2023/03/08 15:00:00</div>
+      <div style="font-size: 5vh">{{ now_datetime }}</div>
     </div>
   </div>
 
+<!-- TODO: コンポーネント分ける -->
   <div class="d-flex align-items-center" style="height: 20vh">
     <div class="text-center w-100">
-      <div class="mt-5" style="font-size: 30vh">09:59:59</div>
+      <div class="mt-5" style="font-size: 30vh">{{ countDown }}</div>
     </div>
   </div>
 
@@ -26,17 +25,59 @@ import NowStatusFooter from './components/NowStatusFooter.vue'
   
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script >
+import NowStatusFooter from './components/NowStatusFooter.vue'
+import dayjs from 'dayjs';
+import duration from 'dayjs/plugin/duration'
+
+dayjs.extend(duration)
+
+export default {
+  data() {
+    return {
+      now_datetime: '',
+      countDown: '',
+    }
+  },
+  mounted() {
+    this.startTimer();
+    setInterval(() => {
+      this.now_datetime = dayjs().format('YYYY/MM/DD HH:mm:ss');
+      
+    }, 1000);
+  },
+  methods: {
+    startTimer() {
+      this.updateCountDown();
+      this.timer = setInterval(() => {
+        this.updateCountDown();
+      }, 1000);
+    },
+    stopTimer() {
+      clearInterval(this.timer);
+      this.timer = null;
+    },
+    updateCountDown() {
+      const now = dayjs();
+      let currentMinute = now.minute();
+      let targetMinute = Math.ceil(currentMinute / 10) * 10;
+      let targetTime = dayjs().minute(targetMinute).second(0);
+      if (targetTime.diff(now) <= 0) {
+        targetTime = targetTime.add(10, 'minute');
+      }
+      let diff = dayjs.duration(targetTime.diff(now));
+      let minutes = diff.minutes().toString().padStart(2, '0');
+      let seconds = diff.seconds().toString().padStart(2, '0');
+      this.countDown = `${minutes}:${seconds}`;
+    },
+  },
+
+
+
+  components: {
+    NowStatusFooter
+  },
+
+
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+</script>
