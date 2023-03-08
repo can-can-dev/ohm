066de8b49ace551c6fd467df0075dcd7205748fa
can <renkon1103@gmail.com>
2023-03-09 06:04:12 +0900

    footer メッセージ実装

--- src/components/NowStatusFooter.vue
+++ src/components/NowStatusFooter.vue
@@ -1,9 +1,3 @@
-<script setup>
-import { ref } from 'vue'
-
-const count = ref(0)
-</script>
-
 <template>
   <div class="fixed-bottom">
     <div class="row">
@@ -12,17 +6,17 @@ const count = ref(0)
         <div class="bg-secondary border-bottom">
           <label class="text-white">オンロード</label>
         </div>
-        <div class="bg-danger text-white d-flex justify-content-center align-items-center" style="height:10vh">
-          上級者
+        <div class="d-flex justify-content-center align-items-center" :class="onload.class" style="height:10vh">
+          {{ onload.text }}
         </div>
       </div>
 
       <div class="col p-0 border">
         <div class="bg-secondary border-bottom">
-          <label class="text-white">オンロード</label>
+          <label class="text-white">オフロード</label>
         </div>
-        <div class="bg-warning d-flex justify-content-center align-items-center" style="height:10vh">
-          初心者
+        <div class="d-flex justify-content-center align-items-center" :class="offload.class" style="height:10vh">
+          {{ offload.text }}
         </div>
       </div>
 
@@ -30,8 +24,65 @@ const count = ref(0)
   </div>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
+<script>
+import dayjs from 'dayjs';
+
+export default {
+  data() {
+    return {
+      offload: {
+        text: '',
+        class: '',
+      },
+      onload: {
+        text: '',
+        class: '',
+      },
+    }
+  },
+  mounted() {
+    setInterval(() => {
+      let now_minits = parseFloat(dayjs().format('m'));
+      let time_group = Math.floor(now_minits * 0.1) % 3;
+
+      switch (time_group) {
+        case 0:
+          this.onload = {
+            text: '上級クラス',
+            class: 'bg-danger text-white',
+          };
+
+          this.offload = {
+            text: '初・中級クラス',
+            class: 'bg-warning',
+          };
+          break;
+        case 1:
+          this.onload = {
+            text: '中級クラス',
+            class: 'bg-primary text-white',
+          };
+
+          this.offload = {
+            text: 'ハイエンド時間　オフストック',
+            class: 'bg-primary text-white',
+          };
+          break;
+        case 2:
+          this.onload = {
+            text: '初・中級クラス',
+            class: 'bg-warning',
+          };
+
+          this.offload = {
+            text: 'ハイエンド時間　モデファイ最優先',
+            class: 'bg-danger text-white',
+          };
+          break;
+        default:
+          console.log(`out of`);
+      }
+    }, 1000);
+  },
 }
-</style>
+</script>
